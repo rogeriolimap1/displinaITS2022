79720070892fa0e50759b6d566ad731bbb4629d4
LG <LG@LG-PC>
2022-09-01 14:38:07 -0300

    Terceira atividade de testes

--- primeiroSpringBoot/src/test/java/org/iftm/primeiroSpringBoot/entity/CalculadoraTest.java
+++ primeiroSpringBoot/src/test/java/org/iftm/primeiroSpringBoot/entity/CalculadoraTest.java
@@ -31,4 +31,82 @@ public class CalculadoraTest {
 		
 		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
 	}
+	
+	@Test
+	public void testaSubtracaoCorretaParaDoisInteirosPositivos() {
+		//definir cenário de teste
+		Calculadora calculadora = new Calculadora();
+		int entradaNumero1 = 10;
+		int entradaNumero2 = 5;
+		int resultadoEsperado = 5;
+
+		calculadora.subtrair(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+	}
+	public void testaSubtracaoCorretaParaDoisInteirosNegativos() {
+		//definir cenário de teste
+		Calculadora calculadora = new Calculadora();
+		int entradaNumero1 = -10;
+		int entradaNumero2 = 5;
+		int resultadoEsperado = -15;
+
+		calculadora.subtrair(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+	}
+	
+	@Test
+	public void testaMultiplicarCorretaParaDoisInteirosPositivos() {
+		//definir cenário de teste
+		Calculadora calculadora = new Calculadora();
+		int entradaNumero1 = 10;
+		int entradaNumero2 = 5;
+		int resultadoEsperado = 50;
+
+		calculadora.multiplicar(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+	}
+	public void testaMultiplicarCorretaParaDoisInteirosNegativos() {
+		//definir cenário de teste
+		Calculadora calculadora = new Calculadora();
+		int entradaNumero1 = -10;
+		int entradaNumero2 = 5;
+		int resultadoEsperado = -50;
+
+		calculadora.multiplicar(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+	}
+	
+	@Test
+	public void testaDividisaoCorretaParaDoisInteirosPositivos() {
+		//definir cenário de teste
+		Calculadora calculadora = new Calculadora();
+		int entradaNumero1 = 10;
+		int entradaNumero2 = 5;
+		int resultadoEsperado = 2;
+
+		calculadora.somar(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+	}
+	public void testaDividisaoCorretaParaDoisInteirosNegativos() {
+		//definir cenário de teste
+		Calculadora calculadora = new Calculadora();
+		int entradaNumero1 = 10;
+		int entradaNumero2 = 0;
+		int resultadoEsperado = -1;
+
+		calculadora.dividir(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+	}
 }
